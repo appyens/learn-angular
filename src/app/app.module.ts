f809a68f3a1a193e74be922768215f156ef9b4d3
appyens <an.anurag@msn.com>
2019-10-31 02:26:51 +0530

    project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import {SuccessalertComponent} from './assignment-1/successalert/successalert.component';
-import {WarningalertComponent} from './assignment-1/warningalert/warningalert.component';
+import { ServerComponent } from './basics/server/server.component';
+import { ServersComponent } from './basics/servers/servers.component';
+import {SuccessalertComponent} from './basics/assignment-1/successalert/successalert.component';
+import {WarningalertComponent} from './basics/assignment-1/warningalert/warningalert.component';
 import {FormsModule} from '@angular/forms';
-import {AssignmentComponent} from './assignment-2/assignment.component';
-import {TestComponent} from './test/test.component';
-import {Assignment3Component} from './assignment-3/assignment3.component';
+import {AssignmentComponent} from './basics/assignment-2/assignment.component';
+import {TestComponent} from './basics/test/test.component';
+import {Assignment3Component} from './basics/assignment-3/assignment3.component';
+import { BasicMainComponent } from './basics/basic-main/basic-main.component';
+import { HeaderComponent } from './project/header/header.component';
+import { RecipesComponent } from './project/recipes/recipes.component';
+import { RecipeListComponent } from './project/recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './project/recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './project/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './project/shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    // bsics
     ServerComponent,
     ServersComponent,
     SuccessalertComponent,
@@ -21,6 +29,15 @@ import {Assignment3Component} from './assignment-3/assignment3.component';
     AssignmentComponent,
     TestComponent,
     Assignment3Component,
+    BasicMainComponent,
+    // project
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+
   ],
   imports: [
     BrowserModule,
---
+++ src/app/basics/basic-main/basic-main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-main',
+  templateUrl: './basic-main.component.html',
+  styleUrls: ['./basic-main.component.css']
+})
+export class BasicMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
